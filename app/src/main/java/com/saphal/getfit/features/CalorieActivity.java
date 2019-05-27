ce2555a8379ab387d08c036562b76adf7c72fed3
Saphal <gsaphal99@gmail.com>
2019-05-27 10:42:59 +0545

    Completed the Menu Layout and added more features Changed the App name

---
+++ app/src/main/java/com/saphal/getfit/features/CalorieActivity.java
@@ -0,0 +1,15 @@
+package com.saphal.getfit.features;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.saphal.getfit.R;
+
+public class CalorieActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calorie);
+    }
+}
