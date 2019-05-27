c4c0411badf5b3b468831623c611a6bcf51deebf
Saphal <gsaphal99@gmail.com>
2019-05-27 13:28:11 +0545

    Fonts Added Monsterrat Connected to Firebase

---
+++ app/src/main/java/com/saphal/getfit/models/CustomFontApp.java
@@ -0,0 +1,15 @@
+package com.saphal.getfit.models;
+
+import android.app.Application;
+
+import com.saphal.getfit.models.TypefaceUtil;
+
+public class CustomFontApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        TypefaceUtil.overrideFont(getApplicationContext(), "SERIF", "fonts/OpenSans-SemiboldItalic.ttf");
+
+    }
+}
