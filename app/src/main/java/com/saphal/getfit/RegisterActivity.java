32bbfee3f727cac399c0f58fceb4aeffda0cc08d
rachana <rachubasnet66@gmail.com>
2019-05-28 12:15:24 +0545

    login and register update

--- app/src/main/java/com/saphal/getfit/RegisterActivity.java
+++ app/src/main/java/com/saphal/getfit/RegisterActivity.java
@@ -8,15 +8,15 @@ import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    private EditText tv_email,tv_username,password,conpassword;
+    private EditText email,username,password,conpassword;
     private Button btn_save;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        tv_email = findViewById(R.id.tv_email);
-        tv_username = findViewById(R.id.tv_username);
+        email = findViewById(R.id.email);
+        username = findViewById(R.id.username);
         password = findViewById(R.id.password);
         conpassword= findViewById(R.id.conpassword);
     }
