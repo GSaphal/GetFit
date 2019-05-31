82bf90405071d7647147bfe60a9150fddf5031c1
rachana <rachubasnet66@gmail.com>
2019-05-31 16:30:10 +0545

    exercise layout

---
+++ app/src/main/java/com/saphal/getfit/features/ExerciseActivity.java
@@ -0,0 +1,31 @@
+package com.saphal.getfit.features;
+
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.widget.TextView;
+
+import com.saphal.getfit.R;
+
+public class ExerciseActivity extends AppCompatActivity {
+
+    private TextView exercises;
+    private CardView abs,chest,biceps,triceps,legs,back;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercise);
+        exercises= findViewById(R.id.exercises);
+
+        abs= findViewById(R.id.abs);
+        chest= findViewById(R.id.chest);
+        biceps= findViewById(R.id.biceps);
+        triceps= findViewById(R.id.triceps);
+        legs= findViewById(R.id.legs);
+        back= findViewById(R.id.back);
+
+        Typeface shadow = Typeface.createFromAsset(getAssets(), "fonts/Shadow.ttf");
+        exercises.setTypeface(shadow);
+    }
+}
